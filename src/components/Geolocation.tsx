134896dcc22fe299b7bc5710c9c946649ada34de
Cherron Simes <cherron@developdbycherron.com>
2021-09-09 16:56:07 -0500

    Fixed logic for geolocation

--- src/components/Geolocation.tsx
+++ src/components/Geolocation.tsx
@@ -1,53 +1,38 @@
 import React, { Component } from "react";
 
 type GeolocationState = {
-  latitude: number,
-  longitude: number
+    latitude: number
+    longitude: number
 }
-
-type GeolocationProps = {
-//comment out?
-}
-
-class Geolocation extends Component<GeolocationProps,GeolocationState> {
-  constructor(props: GeolocationProps) {
-    super(props)
-    this.state = { 
-      latitude: 0,
-      longitude: 0
+class Geolocation extends Component<{}, GeolocationState> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            latitude: 0,
+            longitude: 0
+        }
     }
-}
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      console.log("Latitude is :", position.coords.latitude);
-      console.log("Longitude is :", position.coords.longitude);
+    getLocation = () => {
+            navigator.geolocation.getCurrentPosition(this.setCoords);
+    }
+    setCoords = (position: any) => {
+        this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }
+    componentDidMount() {
+        this.getLocation()
+    }
 
-      
-    });
-
-
-    
-  }
-
-  getLocation = () => {
-      navigator.geolocation.getCurrentPosition(function(this: any, position) {
-      this.setState = ({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      })
-    })
-
-  }
-
-
-  render() { 
-    return (  
-      <div>
-        {this.state.latitude},{this.state.longitude}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {this.state.latitude}, {this.state.longitude}
+            </div>
+        )
+    }
 }
 
 export default Geolocation;
